97585463fc1f4b6f8aa22ae8a59d8015fb649290
VovaBay2002 <90443728+VovaBay2002@users.noreply.github.com>
2022-03-09 19:11:09 +0300

    Update index.php

--- zadanie3/index.php
+++ zadanie3/index.php
@@ -1,71 +1,87 @@
 <?php
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-  if (!empty($_GET['save'])) {
-    // Если есть параметр save, то выводим сообщение пользователю.
-    print('Спасибо, результаты сохранены.');
-  }
-  // Включаем содержимое файла form.php.
-  include('form.php');
-  // Завершаем работу скрипта.
-  exit();
+    // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+    if (!empty($_GET['save'])) {
+        // Если есть параметр save, то выводим сообщение пользователю.
+        print('Спасибо, результаты сохранены.');
+    }
+    // Включаем содержимое файла form.php.
+    include('form.php');
+    // Завершаем работу скрипта.
+    exit();
 }
 // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-
 // Проверяем ошибки.
 $errors = FALSE;
-if (empty($_POST['fio'])) {
-  print('Заполните имя.<br/>');
-  $errors = TRUE;
+if (empty($_POST['name'])) {
+    print('Заполни имя.<br/>');
+    $errors = TRUE;
+}
+if (empty($_POST['email'])) {
+    print('Заполни email.<br/>');
+    $errors = TRUE;
+}
+if (empty($_POST['birth'])) {
+    print('Выбери дату.<br/>');
+    $errors = TRUE;
+}
+if (empty($_POST['gender'])) {
+    print('Выбери пол.<br/>');
+    $errors = TRUE;
+}
+if (empty($_POST['limbs'])) {
+    print('Выбери количество конечностей.<br/>');
+    $errors = TRUE;
+}
+if (empty($_POST['select'])) {
+    print('Выбери суперспособнос(ть/ти).<br/>');
+    $errors = TRUE;
+}
+if (empty($_POST['bio'])) {
+    print('Расскажи о себе.<br/>');
+    $errors = TRUE;
+}
+if (empty($_POST['policy'])) {
+    print('Ознакомся с политикой обработки данных.<br/>');
+    $errors = TRUE;
 }
 
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
 if ($errors) {
-  // При наличии ошибок завершаем работу скрипта.
-  exit();
+    // При наличии ошибок завершаем работу скрипта.
+    exit();
 }
 
 // Сохранение в базу данных.
+$name = $_POST['name'];
+$email = $_POST['email'];
+$date = $_POST['date'];
+$gender = $_POST['gender'];
+$limbs = (int)$_POST['limbs'];
+$policy = $_POST['policy'];
+$powers = implode(',', $_POST['select']);
 
-// $user = 'db';
-// $pass = '123';
-// $db = new PDO('mysql:host=localhost;dbname=test', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$user = 'u47532';
+$pass = '5378974';
+$db = new PDO('mysql:host=localhost;dbname=u47532', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
-// // Подготовленный запрос. Не именованные метки.
-// try {
-//   $stmt = $db->prepare("INSERT INTO application (name) SET name = ?");
-//   $stmt -> execute(array('fio'));
-// }
-// catch(PDOException $e){
-//   print('Error : ' . $e->getMessage());
-//   exit();
-// }
+// Подготовленный запрос. Не именованные метки.
+try {
+    $stmt = $db->prepare("INSERT INTO clients SET name = ?, email = ?, date = ?, gender = ?, limbs = ?, policy = ?");
+    $stmt->execute(array($name, $email, $date, $gender, $limbs, $policy));
+    $user_id = $db->lastInsertId();
+
+    $superpowers = $db->prepare("INSERT INTO powers SET powers = ?, user_id = ? ");
+    $superpowers->execute(array($powers, $power_id));
+} catch (PDOException $e) {
+    print('Error : ' . $e->getMessage());
+    exit();
+}
 
 //  stmt - это "дескриптор состояния".
- 
-//  Именованные метки.
-//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
-//$stmt -> execute(array('label'=>'perfect', 'color'=>'green'));
- 
-//Еще вариант
-/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
-*/
 
 // Делаем перенаправление.
 // Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
