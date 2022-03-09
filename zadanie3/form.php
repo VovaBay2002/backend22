a269c884b1f8e3b7011a3e05b8b134d0ceb676b3
VovaBay2002 <90443728+VovaBay2002@users.noreply.github.com>
2022-03-09 19:46:41 +0300

    Update form.php

--- zadanie3/form.php
+++ zadanie3/form.php
@@ -2,7 +2,7 @@
 <html lang="">
 
 <head>
-    <meta name="viewport" content="width=device.width, intial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8" />
     <link rel="stylesheet" href="./style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
