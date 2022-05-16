8549e3ffde380a9f83249616cd7d1402219d3657
VovaBay2002 <90443728+VovaBay2002@users.noreply.github.com>
2022-05-16 03:34:30 +0300

    Add files via upload

---
+++ zadanie6/update.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+</head>
+
+<body>
+    <div class="form-container">
+        <form method="POST" action="">
+            <div class="input-group">
+                <span class="input-group-text">Имя</span>
+                <input type="text" class="form-control" name="name" placeholder="Ваше имя" />
+            </div>
+            <div class="input-group">
+                <span class="input-group-text">Email</span>
+                <input type="text" class="form-control" name="email" placeholder="example@mail.ru" />
+            </div>
+            <div class="input-group">
+                <span class="input-group-text">Дата рождения</span>
+                <input type="date" class="form-control" placeholder="example@mail.ru" name="date" />
+            </div>
+            <div id="gender-block">
+                <span class="input-group-text block-title">Пол</span>
+                <div class="radios">
+                    <div class="male-radio">
+                        <input class="form-check-input" type="radio" name="gender" value="m" />
+                        <label class="form-check-label" for="male">Мужской</label>
+                    </div>
+                    <div class="female-radio">
+                        <input class="form-check-input" type="radio" name="gender" value="f" />
+                        <label class="form-check-label" for="female">Женский</label>
+                    </div>
+                </div>
+            </div>
+            <div id="limbs-block">
+                <span class="input-group-text block-title">Конечности</span>
+                <div class="radios">
+                    <div class="limbs-radio">
+                        <input class="form-check-input" type="radio" name="limbs" value="1" />
+                        <label class="form-check-label" for="male">1</label>
+                    </div>
+                    <div class="limbs-radio">
+                        <input class="form-check-input" type="radio" name="limbs" value="2" />
+                        <label class="form-check-label" for="female">2</label>
+                    </div>
+                    <div class="limbs-radio">
+                        <input class="form-check-input" type="radio" name="limbs" value="3" />
+                        <label class="form-check-label" for="female">3</label>
+                    </div>
+                    <div class="limbs-radio">
+                        <input class="form-check-input" type="radio" name="limbs" value="4" />
+                        <label class="form-check-label" for="female">4</label>
+                    </div>
+                </div>
+            </div>
+            <select class="form-select form-select-lg mb-2" name="superpowers[]" multiple>
+                <option value="infinity">Бессмертие</option>
+                <option value="walls">Прохождение сквозь стены</option>
+                <option value="levitation">Левитация</option>
+            </select>
+            <div class="input-group">
+                <textarea class="form-control" placeholder="Расскажите о себе..." name="bio"></textarea>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="y" id="policy" name="policy" checked />
+                <label class="form-check-label" for="policy">Согласен с политикой обработки данных.</label>
+            </div>
+            <button class="btn btn-primary" type="submit" id="send-btn">Отправить</button>
+        </form>
+    </div>
+</body>
+
+</html>
