39b38dbd1e55700489d5d56651145f271358b2a5
VovaBay2002 <90443728+VovaBay2002@users.noreply.github.com>
2022-05-16 03:41:52 +0300

    Update form.php

--- zadanie6/form.php
+++ zadanie6/form.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" href="./style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-    <title>Zadanie 5</title>
+    <title>Zadanie 6</title>
 </head>
 
 <body>
